03d5dc16fe59ea7b12970fbbdc731a872b381b7b
Andrey.Tsvetkov <andre.tsvetkov@x5.ru>
2022-03-16 10:20:39 +0300

    HW2 реализация

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,46 @@
 package hw02unpackstring
 
 import (
+	"bytes"
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(src string) (string, error) {
+	if len(src) == 0 {
+		return "", nil
+	}
+
+	var prev rune
+	var buf bytes.Buffer
+
+	for i := 0; i < len(src); i++ {
+		ch := rune(src[i])
+		switch {
+		case unicode.IsDigit(ch):
+			if prev == 0 {
+				return "", ErrInvalidString
+			}
+			if amount := int(ch - '0'); amount != 0 {
+				buf.WriteString(strings.Repeat(string(prev), amount))
+			}
+			prev = 0
+		case ch == '\\':
+			buf.WriteRune(prev)
+			i++
+			prev = rune(src[i])
+		default:
+			if prev != 0 {
+				buf.WriteRune(prev)
+			}
+			prev = ch
+		}
+	}
+	if prev != 0 {
+		buf.WriteRune(prev)
+	}
+	return buf.String(), nil
 }
