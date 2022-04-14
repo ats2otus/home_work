1afc7f62b57817ac3d2598b50c98456b3f9f62df
Andrey.Tsvetkov <andre.tsvetkov@x5.ru>
2022-04-14 19:50:16 +0300

    HW04 реализация кеша и списка

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,16 +11,46 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mu       sync.RWMutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-type cacheItem struct {
-	key   Key
-	value interface{}
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	item, exists := lru.items[key]
+	if !exists {
+		lru.items[key] = lru.queue.PushFront(value)
+		if lru.queue.Len() > lru.capacity {
+			lru.queue.Remove(lru.queue.Back())
+		}
+	} else {
+		item.Value = value
+		lru.queue.MoveToFront(item)
+	}
+	return exists
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	item, ok := lru.items[key]
+	if !ok {
+		return nil, false
+	}
+	lru.queue.MoveToFront(item)
+	return item.Value, true
+}
+
+func (lru *lruCache) Clear() {
+	for k := range lru.items {
+		delete(lru.items, k)
+	}
+	lru.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
