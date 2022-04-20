e6b83ff198cba53d2536fb5f657ce1399bdbea81
Andrey.Tsvetkov <andre.tsvetkov@x5.ru>
2022-04-20 11:09:30 +0300

    HW05 is done

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,9 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"math"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -9,7 +11,49 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+// m < 0 - it means all errors will be ignored
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if m < 0 {
+		m = math.MaxInt
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	queue := make(chan Task, len(tasks))
+	errors := make(chan error, len(tasks))
+
+	for i := 0; i < n; i++ {
+		go worker(ctx, queue, errors)
+	}
+	for i := 0; i < len(tasks); i++ {
+		queue <- tasks[i]
+	}
+	close(queue)
+
+	var totalErrs int
+	for i := 0; i < len(tasks); i++ {
+		if err := <-errors; err != nil {
+			totalErrs++
+			if totalErrs >= m {
+				return ErrErrorsLimitExceeded
+			}
+		}
+	}
+
 	return nil
+}
+
+func worker(ctx context.Context, src chan Task, done chan error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case task, ok := <-src:
+			if !ok {
+				return
+			}
+			done <- task()
+		}
+	}
 }
