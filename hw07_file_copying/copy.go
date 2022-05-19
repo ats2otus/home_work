846283ffe39e603f84ddeef92fa001bf4970dca8
Andrey.Tsvetkov <andre.tsvetkov@x5.ru>
2022-05-19 17:41:12 +0300

    HW07: тесты

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"math"
 	"os"
-	"strings"
 )
 
 var (
@@ -39,9 +38,9 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	fileSize := info.Size()
 	switch {
 	case fileSize == 0:
-		return fmt.Errorf("src:stat: is empty")
+		return ErrUnsupportedFile
 	case offset > fileSize:
-		return fmt.Errorf("offset[%d] is bigger than file size[%d]", offset, fileSize)
+		return ErrOffsetExceedsFileSize
 	}
 	if _, err := ff.Seek(offset, io.SeekStart); err != nil {
 		return err
@@ -49,7 +48,7 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	ft, err := os.Create(toPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("dst:create: %v", err)
 	}
 	defer ft.Close()
 
@@ -61,7 +60,7 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	if _, err := io.CopyN(&pw, ff, limit); err != nil {
 		if err != io.EOF {
-			return err
+			return fmt.Errorf("copy: %v", err)
 		}
 	}
 
@@ -73,26 +72,4 @@ func min(a, b int64) int64 {
 		return a
 	}
 	return b
-}
-
-type ProgressWriter struct {
-	size       int
-	total      int64
-	width      int
-	head, tail string
-	dst, log   io.Writer
-}
-
-func (pw *ProgressWriter) Write(data []byte) (int, error) {
-	write, err := pw.dst.Write(data)
-	pw.size += write
-
-	percent := float64(pw.size*100) / float64(pw.total)
-	middlePoint := int(percent) * pw.width / 100
-
-	head := strings.Repeat(pw.head, middlePoint)
-	tail := strings.Repeat(pw.tail, pw.width-middlePoint)
-	fmt.Fprintf(pw.log, "\r%.2f%%\t[%s%s]", percent, head, tail)
-
-	return write, err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,103 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	root, err := os.MkdirTemp(os.TempDir(), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { os.RemoveAll(root) })
+
+	testCases := []struct {
+		name     string
+		limit    int64
+		offset   int64
+		hasErr   bool
+		src, dst string
+	}{
+		{
+			name:   "negative offset",
+			limit:  -1,
+			hasErr: true,
+		},
+		{
+			name:   "negative offset",
+			offset: -1,
+			hasErr: true,
+		},
+		{
+			name:   "too big offset",
+			offset: math.MaxInt,
+			hasErr: true,
+			src:    "testdata/input.txt",
+		},
+		{
+			name:   "absence src file",
+			hasErr: true,
+			src:    "testdata/not_found.txt",
+		},
+		{
+			name:   "sizeless src file",
+			hasErr: true,
+			src:    "/dev/urandom",
+		},
+		{
+			name:   "unreachable dst file path",
+			hasErr: true,
+			src:    "testdata/input.txt",
+			dst:    "/tmp/hw07/file_copying/dst.txt",
+		},
+		{
+			name:  "with limit",
+			limit: 10,
+			src:   "testdata/input.txt",
+			dst:   path.Join(root, "out.limit.txt"),
+		},
+		{
+			name:   "with offset",
+			offset: 10,
+			src:    "testdata/input.txt",
+			dst:    path.Join(root, "out.offset.txt"),
+		},
+		{
+			name:  "with limit and offset",
+			limit: 10, offset: 10,
+			src: "testdata/input.txt",
+			dst: path.Join(root, "out.limit.offset.txt"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := Copy(tc.src, tc.dst, tc.offset, tc.limit)
+			if tc.hasErr {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(
+					t,
+					mustReadFileData(tc.src, tc.offset, tc.limit),
+					mustReadFileData(tc.dst, 0, 0),
+				)
+			}
+		})
+	}
+}
+
+func mustReadFileData(path string, from, size int64) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	if size == 0 {
+		return string(data[from:])
+	}
+	return string(data[from : from+size])
 }
